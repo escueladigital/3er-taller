495afb6502a86d4423e96cea21d284c9f48a99e6
Escuela Digital <ceo@escueladigital.pe>
2015-10-03 13:41:52 -0500

    4to #edtaller

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,7 @@
 $(function(){
-
+    var $toggleMenu = $('.toggle-menu');
+    var $menu = $('.ed-menu');
+    $toggleMenu.on('click',function(){
+        $menu.toggleClass('mostrar');
+    });
 });
